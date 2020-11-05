07983a92ed9e76d4c0eca6e81206e31dfb156470
Celenny Cristhyne <celennycs@gmail.com>
2020-11-05 17:29:42 -0300

    Update ex08.py

--- exercicios/aula4/ex08.py
+++ exercicios/aula4/ex08.py
@@ -5,3 +5,8 @@ q1.imprimi()
 
 q2 = Quadrado.Quadrado(10,10,10,10)
 q2.imprimi()
+'''
+Exercícios do grupo de estudos de python 
+intermediário da PyLadies Brasil (POO) 
+AULA 4 - polimorfismo + classes abstratas
+'''
